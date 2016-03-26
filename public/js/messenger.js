c6959e563129e99e524e56b2b6a3dfd3b762d01a
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-26 14:08:19 -0400

    working messenage board

--- public/js/messenger.js
+++ public/js/messenger.js
@@ -1,13 +1,16 @@
 //================================================================
 //Sockets functionality
 //================================================================
-console.log('messanger.js is accessed')
-var socket = io();
-$('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
-});
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
-});
+$(function(){
+  console.log('messanger.js is accessed');
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+
+}) //<--windowonload
