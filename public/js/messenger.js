c9244df8d6f59f7bdf441c3e4cc5dd1cfe181ce0
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-30 19:10:22 -0400

    using window.location.hash to move functionality to new page

--- public/js/messenger.js
+++ public/js/messenger.js
@@ -5,21 +5,54 @@ var socket = io();
 $(function(){
   var $convos = $('#convos');
 
-
+var onlineUsers = [];
 
 function addName(name){
 
   socket.on('allClients', function(allClients) {
     console.log(allClients);
-    console.log(name);
+    //defines online user's name
     var userName = name;
-    $(allClients).each(function(index, value) {
-      $convos.append('<li id='+ value + '>' + userName + '</li>');
+    console.log("THIS IS ONLINE USERS", onlineUsers);
 
-      $('#my-friends > li').click(function() {
-        // console.log($(this).prop('id'));
-        socket.emit('socket-id', $(this).prop('id'));
+    //need to make sure a person isn't allowed to be added twice to either client array
+      $(allClients).each(function(index, value) {
+        if($.inArray(userName, onlineUsers) == -1) {
+        //adds user to online user list with socket id as id
+        $convos.append('<li id='+ value + '>' + userName + '</li>');
+        onlineUsers.push(userName);
+        console.log("THIS IS SECOND onlineUsers", onlineUsers);
+      }
+
+      $('#convos > li').click(function() {
+        var onlineUserId = $(this).prop('id');
+        window.location.assign("http://localhost:8080/messanger#chatOnMessanger(" + onlineUserId + ")");
       });
+
+      if(window.location.hash) {
+        console.log("this is the window.location.hash sub: ", window.location.hash.substr(1))
+        eval(window.location.hash.substr(1));
+      }
+
+
+      function chatOnMessanger(onlineUserId){
+        console.log('chatOnMessanger accessed');
+
+        console.log('chatOnMessanger accessed after location');
+        console.log("this is online user's id from onclcik", onlineUserId);
+        socket.emit('socket-id', onlineUserId);
+
+        $('form').submit(function(){
+          socket.emit('chat message', $('#m').val());
+          $('#m').val('');
+          return false;
+        });
+        socket.on('chat message', function(msg){
+          $('#messages').append($('<li>').text(msg));
+        });
+
+      }
+
 
     });
 
@@ -39,12 +72,10 @@ if(true) {
     $.ajax({
       method: 'GET',
       url: '/json',
-      timeout: 4000
+      timeout: 1000
     }).done(
     //success
     function(response){
-      console.log(response);
-      console.log('THIS IS THE NAME FROM getNAME: ', response.userProfile.displayName);
       addName(response.userProfile.displayName);
     },
     //error
@@ -64,30 +95,10 @@ if(true) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // ===========================================
-//
-//   $('form').submit(function(){
-//     socket.emit('chat message', $('#m').val());
-//     $('#m').val('');
-//     return false;
-//   });
-//   socket.on('chat message', function(msg){
-//     $('#messages').append($('<li>').text(msg));
-//   });
-//
+
+
+
 
 
 
